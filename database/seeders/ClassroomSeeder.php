cd3599eacb3f10cf6d11971ffa67831ba9f37795
Faqih Madani <faqihfaqih22@gmail.com>
2025-02-01 00:04:48 +0700

    add seeder

---
+++ database/seeders/ClassroomSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Classroom;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassroomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Classroom::create(['name' => 'Class A']);
+        Classroom::create(['name' => 'Class B']);
+        Classroom::create(['name' => 'Class C']);
+    }
+}
